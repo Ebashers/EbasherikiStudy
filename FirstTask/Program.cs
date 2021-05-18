e7409c0c6a911728efaea1846e22bd7ef710d498
KolaCation <arharovoversteam@gmail.com>
2021-05-18 22:27:09 +0300

    Add project files

---
+++ FirstTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
