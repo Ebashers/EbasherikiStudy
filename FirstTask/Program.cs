d8624e6884eb25a5fd9fbe7e39f748659dd1a39b
Dmytro Kudrenko <dmytro.kudrenko@dedicatedlab.com>
2021-05-18 22:51:54 +0300

    Fix idea

--- FirstTask/Program.cs
+++ FirstTask/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FirstTask.MathModules;
+using System;
+
 
 namespace FirstTask
 {
@@ -6,7 +8,28 @@ namespace FirstTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //code to use "args"
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            //Task1A
+            Console.WriteLine("First task");
+
+            Console.WriteLine("Enter a m variable");
+            double m = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter a k variable");
+            double k = Convert.ToDouble(Console.ReadLine());
+
+            if (k <= 30)
+            {
+                double sumA = Task1A.Calculate(m, k);
+                Console.WriteLine("sum = " + sumA);
+            }
+            else Console.WriteLine("Ooops");
+//end
         }
     }
 }
