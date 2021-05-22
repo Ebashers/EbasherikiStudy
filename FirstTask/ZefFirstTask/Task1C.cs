8a3b4ad6b1bbef942d3818ac2652a26c78698c3e
zefirlover <ibodikmushta@gmail.com>
2021-05-22 19:48:53 +0300

    Task1C and Task1A(KolaCation special)(Test.cs) was added. Task 1C don't work

---
+++ FirstTask/ZefFirstTask/Task1C.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTask.ZefFirstTask
+{
+    public static class Task1C
+    {
+        public static double Calculate(double e, double StartK)
+        {
+            int i = 0;
+            double k;
+            double SumC = 0;
+            for (k = StartK; k > 0; k++)
+            {
+                for (i = 1; i <= k; i++)
+                {
+                    SumC = Math.Pow(-1, i) / Factorial(i);
+                    if (Math.Abs(SumC) < e) break;
+                }
+            }
+            return SumC;
+        }
+        static int Factorial(int i)
+        {
+            if (i == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return i * Factorial(i - 1);
+            }
+        }
+    }
+}
