eaaebb674c3caa77cdd57aee9e9cf9a05d4af0cf
zefirlover <ibodikmushta@gmail.com>
2021-05-22 15:49:59 +0300

    First working prototype of Task 1(b)

--- FirstTask/Program.cs
+++ FirstTask/Program.cs
@@ -8,12 +8,30 @@ namespace FirstTask
         static void Main(string[] args)
         {
             //zefirlover's Task1A
+            Console.WriteLine("First task");
+
             Console.WriteLine("Enter a z variable");
             double z = Convert.ToDouble(Console.ReadLine());
+
             double sum = Task1A.Calculate(z);
             Console.WriteLine("sum = " + sum);
             //zefirlover's Task1A end
 
+            //zefirlover's Task1B
+            Console.WriteLine("Second task");
+
+            Console.WriteLine("Enter a t variable");
+            double t = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter a n variable");
+            double n = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter a l variable");
+            int l = Convert.ToInt32(Console.ReadLine());
+
+            double SumB = Task1B.Calculate(t, n, l);
+            Console.WriteLine("sum = " + SumB);
+            //zefirlover's Task1B end
 
         }
     }
---
+++ FirstTask/ZefFirstTask/Task1B.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTask.ZefFirstTask
+{
+    public static class Task1B
+    {
+        public static double Calculate(double t, double n, int l)
+        {
+            double SumB = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (l%2==0)
+                {
+                    SumB += ((1 / Math.Sqrt(t)) * l);
+                }
+                else
+                {
+                    SumB += ((1 / t) * l);
+                }
+            }
+            return SumB;
+        }
+    }
+}
