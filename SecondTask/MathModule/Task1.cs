dcf4295b8ac7fd468d15ba61b7c2d7efd0855ab8
zefirlover <ibodikmushta@gmail.com>
2021-05-28 21:53:36 +0300

    Task2A is done

---
+++ SecondTask/MathModule/Task1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondTask.MathModule
+{
+    public static class Task1
+    {
+        public static int Calculate(int[] firstArray)
+        {
+            int maxValue = int.MinValue;
+            foreach (int a in firstArray)
+            {
+                if (a > maxValue)
+                {
+                    maxValue = a;
+                }
+            }
+            return maxValue;
+        }
+    }
+}
