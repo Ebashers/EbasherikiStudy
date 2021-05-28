e65003b4f507ffbe4b59a9e688c485c0a9fbd447
zefirlover <ibodikmushta@gmail.com>
2021-05-28 22:15:30 +0300

    Task2B completed

---
+++ SecondTask/MathModule/Task2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondTask.MathModule
+{
+    public static class Task2
+    {
+        public static int Calculate(int[] secondArray)
+        {
+            int maxValue = int.MinValue;
+            foreach (int a in secondArray)
+            {
+                if (a > maxValue)
+                {
+                    maxValue = a;
+                }
+            }
+            int sum = maxValue + secondArray[1];
+            return sum;
+        }
+    }
+}
