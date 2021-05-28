a082bda497904f19dca8bb978d1d069e8ee08358
zefirlover <ibodikmushta@gmail.com>
2021-05-29 00:27:16 +0300

    Task2C (working)

--- SecondTask/MathModule/Task3.cs
+++ SecondTask/MathModule/Task3.cs
@@ -10,35 +10,33 @@ namespace SecondTask.MathModule
     {
         public static int[] Calculate(int[] thirdArray)
         {
-            //int[] fourthArray = thirdArray.ToArray();
+            int[] fourthArray = new int[thirdArray.Length];
 
-            int maxValue = int.MinValue;
-            foreach (int a in thirdArray)
+            int maxValue = thirdArray.Max();
+            //foreach (int a in thirdArray)
+            //{
+            //    if (a > maxValue)
+            //    {
+            //        maxValue = a;
+            //    }
+            //}
+            for (int i = 0; i < thirdArray.Length; i++)
             {
-                if (a > maxValue)
+                //debug variable
+                //var x = thirdArray[i];
+                if (thirdArray[i] == 0)
                 {
-                    maxValue = a;
+                    fourthArray[i] = 1;
+                }
+                else if (thirdArray[i] < 0)
+                {
+                    fourthArray[i] = thirdArray[i] + maxValue;
+                }
+                else if (thirdArray[i] > 0)
+                {
+                    fourthArray[i] = thirdArray[i] * 2;
                 }
             }
-            for (int a = 0; a < thirdArray.Length; a++)
-            {
-                if (a == 0)
-                {
-                    a = 1;
-                    Console.WriteLine(a);
-                }
-                else if (a < 0)
-                {
-                    a += maxValue;
-                    Console.WriteLine(a);
-                }
-                else if (a > 0)
-                {
-                    a *= 2;
-                    Console.WriteLine(a);
-                }
-            }
-            int[] fourthArray = thirdArray.ToArray();
             foreach (int a in fourthArray)
             {
                 Console.WriteLine(a);
