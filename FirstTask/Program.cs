38116f163b6c20d8afef442fb139a69cc80682cf
Victoria Nesterenko <DanilDanill@gmail.com>
2021-05-29 17:42:32 +0300

    Have done Task1

--- FirstTask/Program.cs
+++ FirstTask/Program.cs
@@ -14,13 +14,13 @@ namespace FirstTask
                 throw new ArgumentNullException(nameof(args));
             }
 
-            //Task1A
+            //Task1 - Variant1 
             Console.WriteLine("First task");
 
-            Console.WriteLine("Enter a m variable");
+            Console.WriteLine("Enter m variable");
             double m = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Enter a k variable");
+            Console.WriteLine("Enter k variable");
             double k = Convert.ToDouble(Console.ReadLine());
 
             if (k <= 30)
@@ -28,8 +28,36 @@ namespace FirstTask
                 double sumA = Task1A.Calculate(m, k);
                 Console.WriteLine("sum = " + sumA);
             }
-            else Console.WriteLine("Ooops");
-//end
+            else Console.WriteLine("Wrong k value");
+            //end
+            
+            //Task1B - Variant6
+            Console.WriteLine("Second task");
+
+            Console.WriteLine("Enter t variable");
+            double t = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter i variable");
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            if (t > 0)
+            {
+                double res = Task1B.Calculate(t, i);
+                Console.WriteLine("sum = " + res);
+            }
+            else Console.WriteLine("t has negative value");
+            //end
+
+
+            //Task1C - Variant11
+            Console.WriteLine("Third task");
+
+            Console.WriteLine("Enter E variable");
+            double e = Convert.ToDouble(Console.ReadLine());
+
+            double sum = Task1C.Calculate(e);
+            Console.WriteLine("sum = " + sum);
+            //end
         }
     }
 }
