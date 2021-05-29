628a7b9dac7fa6db7da0a8f5602a8d40b0e2d624
Victoria Nesterenko <DanilDanill@gmail.com>
2021-05-29 17:53:37 +0300

    Task1B + Task1C

---
+++ FirstTask/MathModules/Task1C.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace FirstTask.MathModules
+{
+    public static class Task1C
+    {
+        public static double Calculate(double e)
+        {
+            double i = 0;
+            double sum = 0;
+            double sumInitial;
+            while (true)
+            {
+                sumInitial = 1 / Math.Pow(i, 2);
+                i++;
+                if (Math.Abs(sumInitial) < e) break;
+                sum += sumInitial;
+            }
+
+            return sum;
+        }
+    }
+}
