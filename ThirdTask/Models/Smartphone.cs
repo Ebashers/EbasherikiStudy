dc0e28c59f1c8f77686c0cae46aa10cca0783469
zefirlover <ibodikmushta@gmail.com>
2021-06-06 22:52:56 +0300

    "ThirdTask" solution added Models added

---
+++ ThirdTask/Models/Smartphone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdTask.Models
+{
+    class Smartphone : Telephone
+    {
+        public override string Call()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Charge()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ThirdTask/Models/Telephone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdTask.Models
+{
+    abstract class Telephone
+    {
+        // блять нахуй сука піздєц я не понімаю як з цією хуйнею работать, нахуя тут ктор, нада тут ктор, шо, куда блять, єбаний рот
+        public abstract string Call();
+        public abstract string Charge();
+        public Telephone(string _Call, string _Charge)
+        {
+            _Call = Call().ToString();
+            _Charge = Charge().ToString();
+        }
+    }
+}
