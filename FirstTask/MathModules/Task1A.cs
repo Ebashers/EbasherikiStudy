d89c7032b4a583ea98c0e77cf847c90702e81d38
Victoria Nesterenko <DanilDanill@gmail.com>
2021-06-10 11:47:36 +0300

    Fix Task1A

--- FirstTask/MathModules/Task1A.cs
+++ FirstTask/MathModules/Task1A.cs
@@ -10,7 +10,14 @@ namespace FirstTask.MathModules
             double sumA = 0;
             for (i = 1; i <= k; i++)
             {
-                sumA += ((Math.Sqrt(m * 1 / i)) * Math.Sin(m * i));
+                if (m == 0)
+                {
+                    sumA = 0;
+                }
+                else
+                {
+                    sumA += ((Math.Sqrt(m * 1 / i)) * Math.Sin(m * i));
+                }
             }
             return sumA;
         }
