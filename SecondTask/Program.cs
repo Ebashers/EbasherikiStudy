ba4234e7009cf569633c0a3bbac1c4e8d758bc24
KolaCation <arharovoversteam@gmail.com>
2021-06-12 19:45:05 +0300

    add SecondTask project

---
+++ SecondTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SecondTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
