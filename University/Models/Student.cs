6b5baf04bc77a6e4d1bf9d6fccc7138f7b19400e
zefirlover <ibodikmushta@gmail.com>
2021-06-20 15:22:28 +0300

    Task 4A Models ready

---
+++ University/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int GradebookId { get; set; }    // айді заліковки
+        public int GPA { get; set; }            // середній бал
+    }
+}
