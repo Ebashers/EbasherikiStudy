321e813198941ba83c5f1afabf54d31aab3d3f77
Victoria Nesterenko <DanilDanill@gmail.com>
2021-06-29 19:07:46 +0300

    Fix 2A - 2B

--- Task2/MathModules_2/Task2A.cs
+++ Task2/MathModules_2/Task2A.cs
@@ -1,18 +1,22 @@
 using System;
 
-public static class Task2A
+namespace Task2.MathModules_2
 {
-   
-    public static int numberofPositive(int[] firstArray)
+    public static class Task2A
     {
-      
+
+        public static int numberofPositive(int[] firstArray)
+        {
+
             //returns number of positive elements in arr 
             int res = 0;
-            foreach (int value in firstArray){
-                if(value > 0)
+            foreach (int value in firstArray)
+            {
+                if (value > 0)
                     res++;
             }
-        
+
             return res;
         }
     }
+}
--- Task2/MathModules_2/Task2B.cs
+++ Task2/MathModules_2/Task2B.cs
@@ -1,23 +1,27 @@
 using System;
 
-public static class Task2B
+namespace Task2.MathModules_2
 {
-
-    public static int getMinNumSum(int[] arr)
+    public static class Task2B
     {
-        //returns sum of min positive value of arr and index 
-        //if array do not contains positive numbers, returns 0
-        int min_ind = -1;
-        int etalon = int.MaxValue;
-        for (int i = 0; i < arr.Length; i++)
+
+        public static int getMinNumSum(int[] arr)
         {
-            if (arr[i] > 0 && arr[i] < etalon)
-                min_ind = i;
-            etalon = etalon++;
+            //returns sum of min positive value of arr and index 
+            //if array do not contains positive numbers, returns 0
+            int min_ind = -1;
+            int etalon = int.MaxValue;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 0 && arr[i] < etalon)
+                    min_ind = i;
+                etalon = etalon++;
+            }
+
+            int res = 0;
+            if (min_ind != -1)
+                res = min_ind * arr[min_ind];
+            return res;
         }
-        int res = 0;
-        if (min_ind != -1)
-            res = min_ind * arr[min_ind];
-        return res;
     }
 }
