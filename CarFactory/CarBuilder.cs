9f06eef9e077b8c0cd75d959ecbc7e6a03f7dec7
zefirlover <ibodikmushta@gmail.com>
2021-06-29 14:39:14 +0300

    Builder pattern 1 Project created

---
+++ CarFactory/CarBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFactory
+{
+    abstract class CarBuilder
+    {
+        public Car Car { get; protected set; }
+        public void CreateCar()
+        {
+            Car = new Car();
+        }
+        public abstract void SetEngine();
+        public abstract void SetSuspension();
+        public abstract void SetBody();
+        public abstract void SetOptions();
+    }
+}
