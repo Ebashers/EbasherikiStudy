e5b62f6d7b8fd5b2c4e5b5fd526871b838576669
zefirlover <ibodikmushta@gmail.com>
2021-07-04 16:45:10 +0300

    University NameConvention fix

--- University/Program.cs
+++ University/Program.cs
@@ -13,26 +13,26 @@ namespace University
             var faculty = new MockFaculty().Faculties;
             var students = new MockStudent().Students;
 
-            var MaxStudents = students.Where(students => students.GPA > 95).ToList();
+            var maxStudents = students.Where(students => students.GPA > 95).ToList();
             Console.WriteLine("Students with GPA > 95:");
-            foreach (var student in MaxStudents)
+            foreach (var student in maxStudents)
             {
                 Console.WriteLine(student.Name + " " + student.Surname + " " + student.GPA);
             }
             Console.WriteLine();
 
-            var StudentsConnection = faculty.GroupJoin(students, faculty => faculty.Id, students => students.FacultyId, (faculties, students) => new Faculty { Id = faculties.Id, Name = faculties.Name, Students = students.ToList() });
-            var MaxFaculty = StudentsConnection.Max(faculty => faculty.Students.Count);
-            var MaxFacultyName = StudentsConnection.Where(faculty => faculty.Students.Count == MaxFaculty).ToList();
+            var studentsConnection = faculty.GroupJoin(students, faculty => faculty.Id, students => students.FacultyId, (faculties, students) => new Faculty { Id = faculties.Id, Name = faculties.Name, Students = students.ToList() });
+            var maxFaculty = studentsConnection.Max(faculty => faculty.Students.Count);
+            var maxFacultyName = studentsConnection.Where(faculty => faculty.Students.Count == maxFaculty).ToList();
             Console.WriteLine("Faculties with the biggest amount of students:");
-            foreach (var f in MaxFacultyName)
+            foreach (var f in maxFacultyName)
             {
                 Console.WriteLine(f.Name);
             }
             Console.WriteLine();
 
             Console.WriteLine("All students amount:");
-            var FacultiesConnection = institute.GroupJoin(faculty, institute => institute.Id, faculty => faculty.InstituteId, (institute, faculties) => new Institute { Id = institute.Id, Name = institute.Name, Faculties = faculties.ToList() });
+            var facultiesConnection = institute.GroupJoin(faculty, institute => institute.Id, faculty => faculty.InstituteId, (institute, faculties) => new Institute { Id = institute.Id, Name = institute.Name, Faculties = faculties.ToList() });
             Console.WriteLine(students.Count());
 
             Console.ReadKey();
