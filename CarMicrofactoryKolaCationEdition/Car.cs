4783244a3e5f4174c6d695e94140133c6fe65b17
zefirlover <ibodikmushta@gmail.com>
2021-07-04 20:20:47 +0300

    Fluent Builder preset ready

---
+++ CarMicrofactoryKolaCationEdition/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarMicrofactoryKolaCationEdition
+{
+    public class Car
+    {
+        public bool Engine { get; set; }
+        public bool Body { get; set; }
+        public bool Options { get; set; }
+        public bool Suspension { get; set; }
+    }
+}
