a60449357cd405597d6ef750df1aeebce8857cd1
Nikolai Arkharov <arharovoversteam@gmail.com>
2021-07-12 23:36:35 +0300

    add EF Core support

--- FirstTask/Program.cs
+++ FirstTask/Program.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using FirstTask.DB;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
 
 namespace FirstTask
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await using var context = new MyDbContext();
+            await context.Database.MigrateAsync();
+            var students = await context.Students.ToListAsync();
+            students.ForEach(x => Console.WriteLine($"{x.Id} - {x.FirstName} - {x.LastName}"));
         }
     }
 }
