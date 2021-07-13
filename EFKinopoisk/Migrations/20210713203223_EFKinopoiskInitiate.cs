6bf05a294f8586d9d8ed0999d924a2ce30a88136
zefirlover <ibodikmushta@gmail.com>
2021-07-14 00:17:29 +0300

    EFKinopoisk preview ready

---
+++ EFKinopoisk/Migrations/20210713203223_EFKinopoiskInitiate.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFKinopoisk.Migrations
+{
+    public partial class EFKinopoiskInitiate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Actors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Actors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Films",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Films", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ActorFilms",
+                columns: table => new
+                {
+                    ActorId = table.Column<int>(type: "int", nullable: false),
+                    FilmId = table.Column<int>(type: "int", nullable: false),
+                    Id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActorFilms", x => new { x.ActorId, x.FilmId });
+                    table.ForeignKey(
+                        name: "FK_ActorFilms_Actors_ActorId",
+                        column: x => x.ActorId,
+                        principalTable: "Actors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ActorFilms_Films_FilmId",
+                        column: x => x.FilmId,
+                        principalTable: "Films",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Actors",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Biba" },
+                    { 2, "Boba" },
+                    { 3, "Ebanko" },
+                    { 4, "KolaCation" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Films",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Dva dolboyoba" },
+                    { 2, "Ya ebu sobak" },
+                    { 3, "Super crossover" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ActorFilms",
+                columns: new[] { "ActorId", "FilmId", "Id" },
+                values: new object[,]
+                {
+                    { 1, 1, 0 },
+                    { 2, 1, 0 },
+                    { 3, 2, 0 },
+                    { 1, 3, 0 },
+                    { 2, 3, 0 },
+                    { 3, 3, 0 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActorFilms_FilmId",
+                table: "ActorFilms",
+                column: "FilmId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ActorFilms");
+
+            migrationBuilder.DropTable(
+                name: "Actors");
+
+            migrationBuilder.DropTable(
+                name: "Films");
+        }
+    }
+}
--- EFKinopoisk/Program.cs
+++ EFKinopoisk/Program.cs
@@ -1,12 +1,51 @@
-﻿using System;
+﻿using EFKinopoisk.DB;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace EFKinopoisk
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await using var context = new MyDbContext();
+            await context.Database.MigrateAsync();
+            var actors = await context.Actors.ToListAsync();
+            var actorFilms = await context.ActorFilms.ToListAsync();
+            var films = await context.Films.ToListAsync();
+
+            Console.WriteLine("Actor without film");
+            foreach (var a in actors)
+            {
+                if (!actorFilms.Any(af => af.ActorId == a.Id))
+                {
+                    Console.WriteLine(a.Name);
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Enter the name of the actor");
+            var firstActor = Convert.ToString(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("All actor's films and another films' actors");
+            var selectedFilms = actorFilms.Where(c => c.Actor.Name.ToLower() == firstActor.ToLower()).Select(f => f.FilmId).ToList();
+            foreach (var sf in selectedFilms)
+            {
+                var thisActors = actorFilms.Where(c => c.FilmId == sf).Select(c => c.Actor.Name).ToList();
+                var thisFilms = films.Where(f => f.Id == sf).Select(f => f.Name).ToList();
+                thisFilms.ForEach(film => Console.WriteLine(film));
+                thisActors.ForEach(filmActor => Console.WriteLine(filmActor));
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Film with the biggest amount of actors");
+            var filmCounts = await context.Films.Select(f => f.ActorFilms.Count).ToListAsync();
+            var maxFilm = filmCounts.Max();
+            var maxFilmName = await context.Films.Where(f => f.ActorFilms.Count == maxFilm).ToListAsync();
+            maxFilmName.ForEach(m => Console.WriteLine(m.Name));
+            Console.WriteLine();
         }
     }
 }
