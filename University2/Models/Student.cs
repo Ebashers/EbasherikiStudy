25e8810d355e3ac0576b0e4c2c489748438c0959
zefirlover <ibodikmushta@gmail.com>
2021-07-14 00:18:36 +0300

    University2 was removed

--- University2/Models/Student.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace University2.Models
-{
-    public sealed class Student
-    {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
-}
